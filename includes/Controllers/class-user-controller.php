5706fb09d679bb23be3e1871e676371de96b42d7
mainul-remark <muhammad.ali@remarkhb.com>
2026-07-06 13:48:44 +0600

    * total orders and users count show on /user/me api

--- includes/Controllers/class-user-controller.php
+++ includes/Controllers/class-user-controller.php
@@ -31,8 +31,22 @@ final class UserController extends Controller
             return new WP_Error('herlan_user_missing', __('Authenticated user could not be loaded.', 'herlan-rest-api'), ['status' => 401]);
         }
 
+        $user_data = Response::user($user);
+
+        $user_data['total_orders'] = function_exists('wc_get_customer_order_count')
+            ? (int) wc_get_customer_order_count($user->ID)
+            : 0;
+
+        $user_data['address_count'] = class_exists('Auth_Popup_Address_Manager')
+            ? count(\Auth_Popup_Address_Manager::get_addresses($user->ID))
+            : 0;
+
         return Response::success([
-            'user' => Response::user($user),
+            'user' => $user_data,
         ]);
+
+//        return Response::success([
+//            'user' => Response::user($user),
+//        ]);
     }
 }
