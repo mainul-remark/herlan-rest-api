052bb6784cb5862a9c7d382b4a54c601cbda0478
mainul-remark <muhammad.ali@remarkhb.com>
2026-07-08 13:54:36 +0600

    * total wishlish count add on user me api

--- includes/Controllers/class-user-controller.php
+++ includes/Controllers/class-user-controller.php
@@ -37,6 +37,8 @@ final class UserController extends Controller
             ? (int) wc_get_customer_order_count($user->ID)
             : 0;
 
+        $user_data['wishlist_count'] = $this->get_wishlist_count($user->ID);
+
         $user_data['address_count'] = class_exists('Auth_Popup_Address_Manager')
             ? count(\Auth_Popup_Address_Manager::get_addresses($user->ID))
             : 0;
@@ -48,5 +50,26 @@ final class UserController extends Controller
 //        return Response::success([
 //            'user' => Response::user($user),
 //        ]);
+    }
+
+    private function get_wishlist_count(int $user_id): int
+    {
+        if (! class_exists('TInvWL_Wishlist') || ! class_exists('TInvWL_Product')) {
+            return 0;
+        }
+
+        wp_set_current_user($user_id);
+
+        $wl      = new \TInvWL_Wishlist();
+        $results = $wl->get_by_user_default($user_id);
+
+        if (empty($results) || ! is_array($results)) {
+            return 0;
+        }
+
+        $wishlist = array_shift($results);
+        $wlp      = new \TInvWL_Product($wishlist);
+
+        return (int) $wlp->get_wishlist([], true);
     }
 }
